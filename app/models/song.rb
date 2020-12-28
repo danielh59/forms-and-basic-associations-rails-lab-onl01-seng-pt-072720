4116fcd88a4d2cce453d9b366672d632426f9073
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-28 12:04:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  
+
 def artist_name=(name)
   artist = Artist.find_or_create_by(name: name)
   self.artist = artist
